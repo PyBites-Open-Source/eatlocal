e2c1a3479eeaa46bf844b35159da94e3694b1b5a
Bob Belderbos <bobbelderbos@gmail.com>
2022-01-22 08:39:20 +0100

    use dotenv to manage env variables, more transparent / persistent https://pybit.es/articles/how-to-handle-environment-variables-in-python/ also make the env variables "required" (get() sets them to None)

--- eatlocal/__main__.py
+++ eatlocal/__main__.py
@@ -1,9 +1,14 @@
-from .eatlocal import extract_bite, submit_bite, download_bite
-from .cli import get_args
 import os
 
-USERNAME = os.environ.get("PYBITES_USERNAME")
-PASSWORD = os.environ.get("PYBITES_PASSWORD")
+from dotenv import load_dotenv
+
+from .cli import get_args
+from .eatlocal import extract_bite, submit_bite, download_bite
+
+load_dotenv()
+
+USERNAME = os.environ["PYBITES_USERNAME"]
+PASSWORD = os.environ["PYBITES_PASSWORD"]
 
 
 def main():
