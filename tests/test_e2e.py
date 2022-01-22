282e09e37239b190ce151a4212ce50a0da2b90ba
Bob Belderbos <bobbelderbos@gmail.com>
2022-01-22 09:36:50 +0100

    finished tests

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -1,10 +1,22 @@
 import os
+import shutil
+
+import pytest
 
 from eatlocal.constants import USERNAME, PASSWORD
 from eatlocal.eatlocal import download_bite
 
 
-def test_eatlocal_downloads_correct_zipfile():
+def test_eatlocal_cannot_download_premium_bite_wo_auth(capfd):
     bite_number = 241
+    download_bite(bite_number, "foo", "bar")
+    output = capfd.readouterr()[0]
+    assert "Bite 241 was not downloaded" in output
+
+
+@pytest.mark.parametrize("bite_number", [1, 241, 306])
+def test_eatlocal_downloads_correct_zipfile(bite_number):
     download_bite(bite_number, USERNAME, PASSWORD)
-    assert f"pybites_bite{bite_number}.zip" in os.listdir()
+    bite_dir = str(bite_number)
+    assert os.path.isdir(bite_dir)
+    shutil.rmtree(bite_dir)
