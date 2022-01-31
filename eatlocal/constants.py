f68d6d7bc6295d7605d2d86c1eba200b41349af7
JnyJny <erik.oshaughnessy@gmail.com>
2022-01-31 14:02:08 -0600

    Refactor: collected more constant strings and misc small changes
    - Moved URL and file name formatting strings from
      `eatlocal/eatlocal.py` into `eatlocal/constants.py`.
    - Made sleep intervals configurable with a default value.
    - Added `cleanup` default boolean argument to `extract_bite` to
      optionally remove archive file after unzipping.
    - Refactored button clicking in the `submit_bite` to be DRYer.

--- eatlocal/constants.py
+++ eatlocal/constants.py
@@ -5,4 +5,8 @@ load_dotenv()
 
 USERNAME = os.environ.get("PYBITES_USERNAME", None)
 PASSWORD = os.environ.get("PYBITES_PASSWORD", None)
+
 BITE_URL = "https://codechalleng.es/bites/api/eatlocal/{bite_number}"
+LOGIN_URL = "https://codechalleng.es/login"
+SUBMIT_URL = "https://codechalleng.es/bites/{bite_number}"
+BITE_ZIPFILE = "pybites_bite{bite_number}.zip"
