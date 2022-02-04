afade014a33e39f0657d0dac7a33657e2b69aecb
Russell Helmstedter <rhelmstedter@gmail.com>
2022-02-03 19:32:21 -0800

    moved webdriver functions to their own file

--- eatlocal/eatlocal.py
+++ eatlocal/eatlocal.py
@@ -10,61 +10,18 @@ from time import sleep
 from typing import Union
 from zipfile import ZipFile, is_zipfile
 
-from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+from .pydriver import driver_setup, pybites_login
 
 from rich.layout import Layout
 from rich.live import Live
 from rich.syntax import Syntax
 from rich.panel import Panel
 
-import sys
 from bs4 import BeautifulSoup
 
-from .constants import BITE_URL, BITE_ZIPFILE, LOGIN_URL, SUBMIT_URL
 
-
-def driver_setup(path: Union[str, Path] = None) -> webdriver.Chrome:
-    """Configures a headless Chrome wedriver and returns it.
-
-    If a path is given, it's used to set the driver's default download
-    directory.
-
-    :path: Union[str, Path]
-    :returns: configured webdriver.Chrome
-    """
-
-    path = str(Path(path or Path.cwd()).resolve())
-
-    options = Options()
-    options.add_argument("--headless")
-    options.add_argument("window-size=1920x1080")
-    chrome_prefs = {"download.default_directory": path}
-    options.experimental_options["prefs"] = chrome_prefs
-
-    return webdriver.Chrome(options=options)
-
-
-def pybites_login(driver: webdriver.Chrome, username: str, password: str) -> None:
-    """Authenticate this driver instance with the given credentials.
-
-    :driver: webdriver.Chrome
-    :username: str
-    :password: str
-    :returns: None
-    """
-
-    print("Logging into PyBites")
-    driver.get(LOGIN_URL)
-
-    username_field = driver.find_element(By.ID, "id_username")
-    username_field.send_keys(username)
-    password_field = driver.find_element(By.ID, "id_password")
-    password_field.send_keys(password)
-    password_field.send_keys(Keys.RETURN)
+from .constants import BITE_URL, BITE_ZIPFILE, SUBMIT_URL
 
 
 def find_cached_archive(bite_number: int, path: Union[str, Path] = None) -> Path:
@@ -288,9 +245,5 @@ def display_bite(
     layout["main"]["directions"].update(Panel(instructions, title="Directions"))
     layout["main"]["code"].update(Panel(code, title="Code"))
 
-    def exit_display():
-        if input().lower in ["q", "^["]:
-            sys.exit()
-
     with Live(layout, screen=True):
-        exit_display()
+        input()
