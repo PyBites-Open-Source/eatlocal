36bcc6fa603806021171ed29843f9e213625d62c
Russell Helmstedter <rhelmstedter@gmail.com>
2022-05-04 18:40:49 -0700

    added version test

---
+++ tests/test_cli.py
@@ -0,0 +1,11 @@
+from typer.testing import CliRunner
+
+from eatlocal.__main__ import cli
+
+runner = CliRunner()
+
+
+def test_version():
+    result = runner.invoke(cli, ["--version"])
+    assert result.exit_code == 0
+    assert "0.8.0" in result.stdout
