1f96d0823c9ee9b4bad47e6342fbf36629b6cace
Russell Helmstedter <rhelmstedter@gmail.com>
2022-05-09 18:10:03 -0700

    added test for submitting missing bite

--- tests/test_units.py
+++ tests/test_units.py
@@ -2,14 +2,13 @@
 
 from pathlib import Path
 from shutil import rmtree
-from unittest.mock import patch
 
 import pytest
 
 from eatlocal.eatlocal import display_bite, extract_bite, submit_bite
 
 TESTING_REPO = Path("./tests/testing_repo/").resolve()
-NOT_DOWNLOADED = (3, 23)
+NOT_DOWNLOADED = (426, 499)
 LOCAL_TEST_ZIPS = (101, 102)
 LOCAL_TEST_BITES = (103, 104)
 
@@ -48,6 +47,22 @@ def test_cannot_extract_existing_bite(
     )
     output = capsys.readouterr().out
     assert f"There already exists a directory for bite {bite_number}. " in output
+
+
+@pytest.mark.parametrize("bite_number", NOT_DOWNLOADED)
+def test_cannot_extract_missing_bite(
+    bite_number: int,
+    capsys,
+) -> None:
+    """Checks that an existing bite can not be extracted unless forced."""
+
+    extract_bite(
+        bite_number,
+        dest_path=TESTING_REPO,
+        cache_path="cache",
+    )
+    output = capsys.readouterr().out
+    assert f"Missing ZIP archive for bite {bite_number}" in output
 
 
 @pytest.mark.parametrize("bite_number", LOCAL_TEST_BITES)
@@ -92,3 +107,20 @@ def test_submit_from_nongit_repo(
     )
     output = capsys.readouterr().out
     assert "Not a valid git repo:" in output
+
+
+@pytest.mark.parametrize("bite_number", NOT_DOWNLOADED)
+def test_submit_missing_bite(
+    bite_number: int,
+    testing_config,
+    capsys,
+) -> None:
+    """Attempt to submit from an invalid git repository."""
+    submit_bite(
+        NOT_DOWNLOADED,
+        testing_config["PYBITES_USERNAME"],
+        testing_config["PYBITES_PASSWORD"],
+        testing_config["PYBITES_REPO"],
+    )
+    output = capsys.readouterr().out
+    assert "Did you mean" in output
