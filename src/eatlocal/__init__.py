17571f47c833f39b45066e4956760ab4bbf6488b
Russell Helmstedter <rhelmstedter@gmail.com>
2024-10-14 21:34:23 -0700

    fixed quotes

--- src/eatlocal/__init__.py
+++ src/eatlocal/__init__.py
@@ -2,7 +2,7 @@
 
 import importlib.metadata
 
-__version__ = importlib.metadata.version('eatlocal')
+__version__ = importlib.metadata.version("eatlocal")
 
 if __name__ == "__main__":
     print(__version__)
