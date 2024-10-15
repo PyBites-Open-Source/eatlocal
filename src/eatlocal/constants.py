4abb6380fb63dc3f74fe1f7ce35b9a2f008ec468
Russell Helmstedter <rhelmstedter@gmail.com>
2024-10-14 20:41:45 -0700

    add a rich console to use warnings and suggestions

--- src/eatlocal/constants.py
+++ src/eatlocal/constants.py
@@ -5,3 +5,6 @@ BITE_URL = "https://pybitesplatform.com{bite_name}"
 EXERCISES_URL = "https://pybitesplatform.com/bites/regular/"
 LOGIN_URL = "https://pybitesplatform.com/accounts/auth/login/"
 FZF_DEFAULT_OPTS = "--height 13 --layout=reverse --border rounded --margin=2%,5%,10%,2%"
+
+WARNING = "red"
+SUGGESTION = "yellow"
